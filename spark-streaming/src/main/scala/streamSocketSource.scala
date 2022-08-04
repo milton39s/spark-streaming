450fb482be4853511d53d33ad1225d447dc3b7c6
Milton Sanabria <milton39s_@hotmail.com>
2022-08-04 01:46:58 -0500

    demo socket added

--- spark-streaming/src/main/scala/streamSocketSource.scala
+++ spark-streaming/src/main/scala/streamSocketSource.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object streamRateSource {
+object streamSocketSource {
 
   def main(args: Array[String]): Unit = {
 
@@ -9,33 +9,45 @@ object streamRateSource {
     val spark = SparkSession
       .builder()
       .master("local")
-      .appName("Rate Source")
+      .appName("Socket Source")
       .getOrCreate()
 
     // Set Spark logging level to ERROR to avoid various other logs on console.
     spark.sparkContext.setLogLevel("ERROR")
 
+    // Define host and port number to Listen.
+    val host = "127.0.0.1"
+    val port = "9999"
+
     // Create Streaming DataFrame by reading data from socket.
     val initDF = spark
       .readStream
-      .format("rate")
-      .option("rowsPerSecond", 1)
+      .format("socket")
+      .option("host", host)
+      .option("port", port)
       .load()
 
     // Check if DataFrame is streaming or Not.
     println("Streaming DataFrame : " + initDF.isStreaming)
 
     // Print Schema of DataFrame
-    println("Schema : ")
-    initDF.printSchema()
+    println("Schema of DataFame initDF ...")
+    println(initDF.printSchema())
 
-    val resultDF = initDF
-      .withColumn("result", col("value") + lit(1))
+    // Perform word count on streaming DataFrame
+    val wordCount = initDF
+      .select(explode(split(col("value"), " ")).alias("words"))
+      .groupBy("words")
+      .count()
 
-    // write output on console
-    resultDF
+    // Print Schema of DataFrame
+    println("Schema of DataFame wordCount ...")
+    println(wordCount.printSchema())
+
+    // Printout the data to console.
+    wordCount
       .writeStream
-      .outputMode("append")
+      .outputMode("complete") // Try "update" and "complete" mode.
       .option("truncate", false)
       .format("console")
       .start()
